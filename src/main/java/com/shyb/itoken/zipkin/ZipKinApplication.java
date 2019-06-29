04780d0e429205c52ed72c3143098ce9f41e36e8
17396387378 <515030185@qq.com>
2019-06-29 10:07:06 +0800

    基本开发环境版

---
+++ src/main/java/com/shyb/itoken/zipkin/ZipKinApplication.java
@@ -0,0 +1,19 @@
+package com.shyb.itoken.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * @author wzh
+ * @date 2019/6/28 - 10:25
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class,args);
+    }
+}
